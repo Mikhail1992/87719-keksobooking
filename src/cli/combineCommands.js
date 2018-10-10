9260da1755db1da4eb291162515a1ebb366754ca
mikhail_dziarkach <misderkach@yandex.ru>
2018-10-10 08:53:01 +0300

    Добавляет запись в файл

--- src/cli/combineCommands.js
+++ src/cli/combineCommands.js
@@ -1,11 +1,15 @@
 'use strict';
 const commands = require(`./`);
 
-module.exports = function (argument) {
-  const command = commands.find((item) => `--${item.name}` === argument);
+module.exports = function (params) {
+  const command = commands.find((item) => `--${item.name}` === params[0]);
 
   if (command) {
-    command.execute(commands);
+    if (command.name === `help`) {
+      command.execute(commands);
+    } else {
+      command.execute(...params.filter((item, index) => index > 0));
+    }
   } else {
     commands.find((item) => item.name === `undef`).execute();
     commands.find((item) => item.name === `help`).execute(commands);
--- src/cli/help.js
+++ src/cli/help.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const colors = require(`colors`);
+const a = require(`./index`);
+console.log(`a`, a);
 
 const findCommand = (commands, command) => {
   return commands.find((item) => item.name === command);
@@ -19,12 +21,13 @@ module.exports = {
   description: `Shows all commands`,
   execute(commands) {
     console.log(`
-      Доступные команды:
+      Available commands:
       ${colors.grey(`--${this.name}`)} — ${this.description.green};
       ${getCommandName(commands, `version`)} — ${getCommandDescr(commands, `version`)};
       ${getCommandName(commands, `author`)} — ${getCommandDescr(commands, `author`)}
       ${getCommandName(commands, `license`)} — ${getCommandDescr(commands, `license`)}
       ${getCommandName(commands, `description`)} — ${getCommandDescr(commands, `description`)}
+      ${getCommandName(commands, `generate`)} — ${getCommandDescr(commands, `generate`)}
     `);
   }
 };
